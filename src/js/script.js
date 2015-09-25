a032fdd569eeb46fd41f2067f6aea943642ba143
norbert <n.nagy@replyltd.co.uk>
2015-09-25 22:43:02 +0100

    remove no match if the next query is successful

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,7 @@
 				$scope.fetch = function() {
 					$scope.searchUrl = 'https://api.foursquare.com/v2/venues/explore?client_id=MXTT00M3AK5GRDZSSDKJN1TDWHBMS3CUAWSVA03YTTKLRB4M&client_secret=JMN2VC3HHOVPJDOTPCNYQGNB23SSHNNW0ENRPS4AJ4PWWWR4&v=20130815&limit=15&near='+$scope.place;
 					$scope.response = null;
+					$scope.nomatch = false;
 
 					if ($scope.place){
 						$http({method: $scope.method, url: $scope.searchUrl, cache: $templateCache}).
