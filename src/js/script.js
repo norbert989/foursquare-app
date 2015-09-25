f44bebd3b829ca802289947940e631e28e5ca5fb
norbert <n.nagy@replyltd.co.uk>
2015-09-25 22:41:39 +0100

    Fix - added bad request 'No Match' message

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@
 		.controller('FetchController', ['$scope', '$http', '$templateCache',
 			function($scope, $http, $templateCache) {
 				$scope.method = 'GET';
+				$scope.nomatch = false;
 
 				$scope.fetch = function() {
 					$scope.searchUrl = 'https://api.foursquare.com/v2/venues/explore?client_id=MXTT00M3AK5GRDZSSDKJN1TDWHBMS3CUAWSVA03YTTKLRB4M&client_secret=JMN2VC3HHOVPJDOTPCNYQGNB23SSHNNW0ENRPS4AJ4PWWWR4&v=20130815&limit=15&near='+$scope.place;
@@ -15,10 +16,14 @@
 								$scope.status = response.status;
 								$scope.data = response.data;
 								$scope.items = $scope.data.response.groups[0].items;
-								console.log($scope.items);
 							}, function(response) {
-								$scope.data = response.data || "Request failed";
-								$scope.status = response.status;
+								console.log(response.status);
+								if (response.status === 400){
+									$scope.items = [];
+									$scope.nomatch = true;
+								}else {
+									$scope.nomatch = false;
+								}
 							});
 					}
 				};
